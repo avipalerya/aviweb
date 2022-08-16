586b17c96cde480d355edb543a4cf690b8513b8b
Avi Palerya <107020@tfcorp.com>
2022-08-16 12:14:31 +0530

    const and key files added

---
+++ Javascript/consandlet.js
@@ -0,0 +1,23 @@
+console.log(student);
+var student = "AVI";
+var student = 34;
+console.log(student);
+// to avoid these conflicts we have let and const key words
+// are intoduced into ES6
+var d;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+var a = 10;
+var b = 20;
+var c = 30;
+console.log(a);
+console.log(b);
+console.log(c);
+// let and const
+//console.log(sec);
+// temporal dead zone-time between var declaration and var initialization
+//we cant access the var untill its assigned with some value
+var sec = "b";
+console.log(sec);
