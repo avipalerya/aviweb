d90142e060ae537e972984695a979c0fc44e713e
avipalerya <avipaleriya27@gmail.com>
2022-08-19 17:41:05 +0530

    windows files added

---
+++ Javascript/window.js
@@ -0,0 +1,8 @@
+//window object is the global objecj
+var x=10;
+m=20;
+console.log(window);
+console.log(window.history);
+console.log(window.location);
+console.log(window.document);
+console.log(this===window);
